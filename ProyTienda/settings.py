67679aefc5beba1031236abd5bfb8324b845d990
caballerofrog <jhoelegus357@gmail.com>
2025-08-19 19:39:43 -0500

    cambio en setting allowed host

--- ProyTienda/settings.py
+++ ProyTienda/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s6^hxni#x$kffhy83k&$(fh(-gezwd22n3-jclz+@@x-t!*!!b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
